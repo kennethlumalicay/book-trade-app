3e3cec9d41bb83133c92e91020f010e41693490a
kennethlumalicay <kennethlumalicay@gmail.com>
2017-10-28 03:10:14 -0400

    removed just kidding

--- dev/js/components/Main.js
+++ dev/js/components/Main.js
@@ -8,7 +8,7 @@ class Main extends Component {
 	    	<div id="main-content" className='flex-col flex-center'>
 	    		<div id='todo' className='flex-row flex-center flex-wrap'>
 	    			<p>Wanna trade your book? Add it in your account for other people to see!</p>
-	    			<p>Browse books posted by thousands of other users. (just kidding)</p>
+	    			<p>Browse books posted by other users.</p>
 	    			<p>Saw a book you like? Click the book to request a trade!</p>
 	    		</div>
 	    		<a id="login-btn" href='/login'>Start trading.</a>
